0e2b00dbccfd67dde76cd2d099924f8d9c4213e5
Ibrahim Kholil <33357137+ibrahimkholil6210@users.noreply.github.com>
2022-08-16 20:29:46 +0600

    chore: home page and add to cart logic

--- src/App.vue
+++ src/App.vue
@@ -12,21 +12,50 @@
         <span>Past Orders</span>
       </router-link>
     </nav>
-    <div class="top-bar-cart-link">
+    <div @click="toogleSidebar" class="top-bar-cart-link">
       <i class="icofont-cart-alt icofont-1x"></i>
-      <span>Cart (0)</span>
+      <span>Cart ({{ totalItem }})</span>
     </div>
   </header>
-  <router-view />
-  <side-bar />
+  <router-view :inventory="inventory" :addToCart="addToCart" />
+  <SideBar v-if="showSidebar" :toggle="toogleSidebar" :cart="cart" :inventory="inventory" :removeItem="removeItem" />
 </template>
 
 <script>
-import Sidebar from '@/components/SideBar.vue'
+import SideBar from '@/components/SideBar.vue'
+import food from '@/data/food.json'
 
 export default {
   components: {
-    'side-bar': Sidebar
+    SideBar
+  },
+  data () {
+    return {
+      showSidebar: false,
+      inventory: food,
+      cart: {}
+    }
+  },
+  computed: {
+    totalItem () {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr
+      }, 0)
+    }
+  },
+  methods: {
+    toogleSidebar () {
+      this.showSidebar = !this.showSidebar
+    },
+    addToCart (name, quantity) {
+      if (!this.cart[name]) {
+        this.cart[name] = 0
+      }
+      this.cart[name] += quantity
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    }
   }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,60 +1,32 @@
 <template>
   <div class="splash-container">
-        <div class="splash">
-          <h1>Splendid Food</h1>
-        </div>
-      </div>
+    <div class="splash">
+      <h1>Splendid Food</h1>
+    </div>
+  </div>
 
-      <main class="wrapper">
+  <main class="wrapper">
 
-        <h2>Recommended</h2>
+    <h2>Recommended</h2>
+    <div class="recommended">
+      <ProductCard v-for="(product, index) in inventory.slice(0, 3)" :key="index" :product="product" :index="index"
+        :addToCart="addToCart" />
+    </div>
 
-        <div class="recommended">
-
-          <div class="card">
-            <div class="card-title">
-              Carrots
-            </div>
-            <div class="card-body">
-              <i class="icofont-10x icofont-carrot"></i>
-              <form>
-                <div class="row">
-                  <div class="cell">
-                    <label>Type:</label>
-                  </div>
-                  <div class="cell">
-                    <em>Vegetable</em>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="cell">
-                    <label>Price:</label>
-                  </div>
-                  <div class="cell">
-                    $4.82
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="cell">
-                    <label>Quantity:</label>
-                  </div>
-                  <div class="cell">
-                    <input type="number">
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div class="card-footer">
-              <button @click="addToCart('carrots')" class="btn btn-light">
-                Add to cart
-              </button>
-            </div>
-          </div>
-
-        </div>
-
-      </main>
+  </main>
 </template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    ProductCard
+  },
+  props: ['inventory', 'addToCart']
+}
+</script>
 
 <!-- <script>
 // @ is an alias to /src
